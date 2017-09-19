38909ee6b28c8c3d80f33c28eec350764891254d
bwyfed <36172659@qq.com>
2017-09-19 09:10:05 +0800

    只使用html文件来加载模块，会导致加载模块失败。

--- static/src/js/app.js
+++ static/src/js/app.js
@@ -2,3 +2,7 @@
 /**
  * Created by BWY on 2017/9/19.
  */
+require(['helper'],function(helper) {
+    var str = helper.trim('   amd    ');
+    console.log(str);
+})
---
+++ static/src/js/helper.js
@@ -0,0 +1,10 @@
+/**
+ * Created by BWY on 2017/9/19.
+ */
+define('helper',['jquery'],function($){
+    return {
+        trim: function(str) {
+            return $.trim(str);
+        }
+    }
+})
