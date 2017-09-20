3d9d77cdca26e3872e7b9d67280c4deada27903c
bwyfed <36172659@qq.com>
2017-09-20 09:07:17 +0800

    增加paths配置引用不在baseUrl下的模块。

--- static/src/js/app.js
+++ static/src/js/app.js
@@ -3,7 +3,13 @@
  * Created by BWY on 2017/9/19.
  */
 requirejs.config({
-    baseUrl: '../src/js'
+    baseUrl: '../src/js',
+    paths: {
+        'jquery': [
+            'http://code.jquery.com/jquery-3.2.1',
+            './lib/jquery'
+        ]
+    }
 })
 
 require(['helper'],function(helper) {
--- static/src/js/helper.js
+++ static/src/js/helper.js
@@ -1,11 +1,8 @@
 /**
  * Created by BWY on 2017/9/19.
  */
-define('helper',['./lib/jquery'],function($){
+define('helper',['jquery'],function($){
     console.log($);
-    require(['jquery'],function($){
-        console.log($);
-    });
     return {
         trim: function(str) {
             return $.trim(str);
