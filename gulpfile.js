b052a8a1654787b5434dccc73357519dd0c0a279
bwyfed <36172659@qq.com>
2017-10-09 17:34:57 +0800

    使用gulp-requirejs和gulp-requirejs-optimize两个gulp插件尝试使用r.js打包项目。

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+/**
+ * Created by BWY on 2017/10/9.
+ */
+const gulp = require('gulp');
+const gulpLoadPlugins = require('gulp-load-plugins');
+
+const $ = gulpLoadPlugins();
+
+gulp.task('rjs',()=>{
+    console.log($.requirejsOptimize);
+    return gulp.src(['static/src/js/app.js','static/src/js/user.js'])
+        .pipe($.requirejsOptimize({
+            mainConfigFile: './static/src/js/require.conf.js'
+        }))
+            /*
+        .pipe($.requirejsOptimize({
+            // appDir: './static/src',
+            baseUrl: './static/src/js',
+            // out: './static/build',
+            optimize: 'none',
+            mainConfigFile: './static/src/js/require.conf.js',
+            inlineText: false,
+            name: 'app',
+            // exclude: ['backbone'],
+            // excludeShallow: ['backbone'],
+            // include: ['modernizr'],
+            // insertRequire: ['lib/modernizr']
+        }))
+        */
+        .pipe(gulp.dest('./static/build'))
+});
+
+gulp.task('build:js',()=>{
+    return $.requirejs({
+        baseUrl: './static/src/js',
+        out: './static/build',
+        optimize: 'none',
+        mainConfigFile: './static/src/js/require.conf.js',
+        inlineText: false,
+        name: 'app',
+        insertRequire: ['lib/modernizr']
+    })
+        // .pipe(gulp.dest('./static/build'))
+});
